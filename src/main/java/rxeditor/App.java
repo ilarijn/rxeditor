eeb8e49289fa9224e9732c0e470e663e55b00bda
Ilari Junnonen <ilari.junnonen@iki.fi>
2020-07-25 00:11:22 +0300

    Fix unlaunchable jar

---
+++ src/main/java/rxeditor/App.java
@@ -0,0 +1,22 @@
+package rxeditor;
+
+import rxeditor.UI.MainWindow;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("rxeditor");
+        MainWindow rxeditor = new MainWindow(primaryStage);
+        primaryStage.setScene(rxeditor.getScene());
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
--- src/main/java/rxeditor/Main.java
+++ src/main/java/rxeditor/Main.java
@@ -1,21 +1,12 @@
 package rxeditor;
 
-import rxeditor.UI.MainWindow;
-import javafx.application.Application;
-import static javafx.application.Application.launch;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("rxeditor");
-        MainWindow rxeditor = new MainWindow(primaryStage);
-        primaryStage.setScene(rxeditor.getScene());
-        primaryStage.show();
-    }
+/**
+ * Main class to call the actual launcher class. If the main class inherits
+ * Application (JavaFX), the jar won't run.
+ */
+public class Main {
 
     public static void main(String[] args) {
-        launch(args);
+        App.main(args);
     }
 }
